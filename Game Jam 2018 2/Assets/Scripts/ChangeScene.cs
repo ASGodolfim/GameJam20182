a0b30fe110a8f1ba8a9c0011c6cd8bb4f19f5c51
Arthur Stone <stonegodolfim@edu.univali.br>
2018-09-22 09:41:31 -0300

    UPDATE

---
+++ Game Jam 2018 2/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void StartCreditos()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void StartPause()
+    {
+        SceneManager.LoadScene("Pause");
+    }
+}
